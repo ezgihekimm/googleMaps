1b28e69bd2bfaccda66876bd85baa87e871a39af
ezgi.hekim.2000 <ezgi.hekim.2000@gmail.com>
2024-02-15 14:54:59 +0300

    Initial Commit

---
+++ maps/mapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapsApp.swift
+//  maps
+//
+//  Created by Ezgi Hekim on 15.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct mapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
